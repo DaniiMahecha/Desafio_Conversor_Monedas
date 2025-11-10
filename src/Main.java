76a25eb433180bc71cb85cee09f4bd679a231443
DaniiMahecha <danielfelipemahechapena1010@gmail.com>
2025-11-09 20:16:37 -0500

    Tried of deserialization

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
-import com.daniidev.request.Request;
+import com.daniidev.request.request.Deserialization;
+import com.daniidev.request.request.Request;
 
 public class Main {
     public static void main(String[] args) {
         Request request = new Request();
-        System.out.println(request.callApi("USD", "COP"));
+        System.out.println(request.callApi("EUR", "COP"));
+        Deserialization deserialization = new Deserialization(request.callApi("COP", "USA"));
+        System.out.println(deserialization);
     }
 }
--- src/com/daniidev/request/request/Request.java
+++ src/com/daniidev/request/request/Request.java
@@ -1,4 +1,6 @@
-package com.daniidev.request;
+package com.daniidev.request.request;
+
+import com.google.gson.Gson;
 
 import java.io.IOException;
 import java.net.URI;
@@ -12,15 +14,15 @@ public class Request {
     String target;
 
 
-    public String callApi(String base, String target){
+    public DTO callApi(String base, String target){
         this.base = base;
         this.target = target;
-        String url = "https://v6.exchangerate-api.com/v6/" + apiKey + "/pair/" + base + "/" + target;
+        URI url = URI.create("https://v6.exchangerate-api.com/v6/" + apiKey + "/pair/" + base + "/" + target);
 
         HttpClient client = HttpClient.newHttpClient();
         // Creando la solicitud
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(url))
+                .uri(url)
                 .GET()
                 .build();
 
@@ -33,6 +35,7 @@ public class Request {
                 throw new RuntimeException(e);
             }
         }
-        return response.body();
+        //Deserializo al DTO el JSON
+        return new Gson().fromJson(response.body(), DTO.class) ;
     }
 }
